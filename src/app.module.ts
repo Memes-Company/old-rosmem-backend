0a1e4e5d277e85a4e05ae4f21643c51a95f359cf
Vaagn Avanesyan <vahagn1993@gmail.com>
2020-04-10 01:35:54 +0300

    Add typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PersistenceModule } from './persistence/persistence.module';
 
 @Module({
-  imports: [PersistenceModule],
+  imports: [TypeOrmModule.forRoot(), PersistenceModule],
   controllers: [AppController],
   providers: [AppService],
 })
