0f42089ed842ef9b42a05cba79141f72689d03cc
Vaagn Avanesyan <vahagn1993@gmail.com>
2020-04-11 17:01:56 +0300

    Connect GraphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PersistenceModule } from './persistence/persistence.module';
 import { AuthModule } from './auth/auth.module';
+import { PersistenceModule } from './persistence/persistence.module';
+import { join } from 'path';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), PersistenceModule, AuthModule],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    TypeOrmModule.forRoot(),
+    PersistenceModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
