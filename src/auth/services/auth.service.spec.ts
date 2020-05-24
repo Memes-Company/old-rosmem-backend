271dd986949dad0aee9fed6b32a759c7a0a0027e
Vaagn Avanesyan <vahagn1993@gmail.com>
2020-05-25 02:41:56 +0300

    Add AuthService tests

--- src/auth/services/auth.service.spec.ts
+++ src/auth/services/auth.service.spec.ts
@@ -1,18 +1,57 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UserRepository } from '../repositories';
+import { JwtService } from '@nestjs/jwt';
+import { AuthCredentialsDto } from '../dtos';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let userRepository: UserRepository;
+  const authToken = 'auth-token';
+  const authDto: AuthCredentialsDto = {
+    login: 'test-login',
+    password: 'test-password',
+  };
+
+  const mockUserRepositoryFactory = () => ({
+    isPasswordValid: jest.fn(() => true),
+    signUp: jest.fn(),
+  });
+
+  const mockJwtService = () => ({
+    signAsync: jest.fn(() => authToken),
+  });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: JwtService, useFactory: mockJwtService },
+        {
+          provide: UserRepository,
+          useFactory: mockUserRepositoryFactory,
+        },
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
+    userRepository = module.get<UserRepository>(UserRepository);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
+  });
+
+  it('should use repository for sign up', async () => {
+    expect(userRepository.signUp).not.toBeCalled();
+    await authService.signUp(authDto);
+    expect(userRepository.signUp).toBeCalledWith(authDto);
+  });
+
+  it('should check password', async () => {
+    expect(userRepository.isPasswordValid).not.toBeCalled();
+    const token = await authService.signIn(authDto);
+    expect(token).toBe(authToken);
+    expect(userRepository.isPasswordValid).toBeCalledWith(authDto);
   });
 });
